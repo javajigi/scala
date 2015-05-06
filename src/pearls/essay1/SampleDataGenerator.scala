f3920b95e9831974ca37be630094ced47cae9140
parkjaesung <javajigi@gmail.com>
2015-05-06 21:57:39 +0900

    2개의 파일로 분리하는 샘플과 분리한 샘플을 정렬하고 합치는 예제 코드 추가함.

--- src/pearls/essay1/SampleDataGenerator.scala
+++ src/pearls/essay1/SampleDataGenerator.scala
@@ -6,11 +6,28 @@ import scala.util.Random
 import pearls.support.Utils
 
 object SampleDataGenerator {
-  def main(args: Array[String]): Unit = {
-    val shuffles = Random.shuffle(Range(1000000, 9999999).toList)
+	val prefix = "resources/pearls/essay1/"
+	
+	def generate(file: String) {
+		val shuffles = Random.shuffle(Range(1000000, 9999999).toList)
     val splitedShuffles = shuffles.take(1000000)
-    Utils.printToFile(new File("resources/pearls/essay1/inputdata.txt")) {
-      p => splitedShuffles.foreach(p.println)
+    printToFile(prefix + file, splitedShuffles)
+	}
+	
+	private def printToFile(file: String, numbers: List[Int]) {
+		Utils.printToFile(new File(file)) {
+      p => numbers.foreach(p.println)
     }
+	}
+	
+	def generateSplitedFile() {
+		val shuffles = Random.shuffle(Range(1000000, 9999999).toList)
+    printToFile(prefix + "splitedData1.txt", shuffles.take(1000000))
+    printToFile(prefix + "splitedData2.txt", shuffles.drop(1000000).take(1000000))
+	}
+	
+  def main(args: Array[String]): Unit = {
+  	// generate("inputdata.txt")
+  	generateSplitedFile()
   }
 }
