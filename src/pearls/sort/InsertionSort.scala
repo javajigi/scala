d6ae4498aacddcd9dfcd197cfd32374f6adbdd67
javajigi <javajigi@gmail.com>
2015-05-07 14:18:27 +0900

    insertion sort 소스 코드 추가함.

---
+++ src/pearls/sort/InsertionSort.scala
@@ -0,0 +1,21 @@
+package pearls.sort
+
+object InsertionSort {
+  def sort(values: Array[Int]): Array[Int] = {
+    for (i <- 1 until values.length) {
+    	val currentVal = values(i)
+      var position = i - 1
+      while (position >= 0 && values(position) > currentVal) {
+    	  values(position + 1) = values(position)
+ 			  position = position - 1
+      }
+      values(position + 1) = currentVal
+    }
+    values
+  }
+  
+  def main(args: Array[String]): Unit = {
+    val sortedValues = sort(Array(3, 2, 6, 0, 9, 8, 1))
+    sortedValues.foreach(println)
+  }
+}
